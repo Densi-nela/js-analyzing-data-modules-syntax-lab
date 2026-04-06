5c2df162c967bd7809386a454286e1d7d58de875
densinela <densinela@gmail.com>
2026-04-06 13:38:37 +0300

    has been merged

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+require("datejs");
+function combineUsers(...args) {
+  const combinedObject = {
+    users: [],
+  };
+  for (i = 0; i < args.length; i++) {
+    const userArray = args[i];
+    combinedObject.users.push(...userArray);
+  }
+  combinedObject.merge_date = new Date().toString("M/d/yyyy");
 
-
+  return combinedObject;
+}
 
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+  ...(typeof combineUsers !== "undefined" && { combineUsers }),
 };
